fc0968b741a347816ce5cf3dfb0dcdf3a9a5788d
Abdulkerim Seid <97250721+Abdulkerim0966@users.noreply.github.com>
2023-12-06 20:10:57 +0300

    Added solution for Partition Array According to Given Pivot

---
+++ week 1/partition-array-according-to-given-pivot.py
@@ -0,0 +1,16 @@
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        less=[]
+        great=[]
+        equal=[]
+        for i in range(len(nums)):
+            if nums[i]<pivot:
+                less.append(nums[i])
+            elif nums[i]>pivot:
+                great.append(nums[i])
+            else:
+                equal.append(nums[i])
+        ans=less+equal+great
+        return ans
+
+        
