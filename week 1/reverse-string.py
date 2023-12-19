5341d5b4083074663e0ef14b8261394f0dcce8df
Abdulkerim Seid <97250721+Abdulkerim0966@users.noreply.github.com>
2023-12-19 20:09:15 +0300

    Added solution for Reverse String

---
+++ week 1/reverse-string.py
@@ -0,0 +1,16 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        lowe=0
+        upp=len(s)-1
+        for i in range (len(s)//2):
+            s[lowe],s[upp]=s[upp],s[lowe]
+            lowe+=1
+            upp-=1
+
+
+          
+
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        
