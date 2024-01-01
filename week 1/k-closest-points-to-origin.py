d16cc7ef667ec5f3880390424dd39fd24724c1bf
Abdulkerim Seid <97250721+Abdulkerim0966@users.noreply.github.com>
2024-01-01 18:05:36 +0300

    Added solution for K Closest Points to Origin

---
+++ week 1/k-closest-points-to-origin.py
@@ -0,0 +1,10 @@
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        p=[0,0]
+        points.sort(key=lambda x:math.dist(p,x))
+
+        return points[0:k]
+    
+        
+
+        
