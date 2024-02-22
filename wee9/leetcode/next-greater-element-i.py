65fa95492275ade99f947a50839a3dd8b1913bad
Abdulkerim Seid <97250721+Abdulkerim0966@users.noreply.github.com>
2024-02-22 15:11:18 +0300

    Add solution for Next Greater Element I

--- wee9/leetcode/next-greater-element-i.py
+++ wee9/leetcode/next-greater-element-i.py
@@ -1,18 +1,16 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        ans=[]
-        for i in range (len(nums1)):
-            n=nums2.index (nums1[i])
-            if n==len(nums2)-1:
-                ans.append (-1)
-
-
-            for j in range (n+1,len(nums2),1):
-                if nums1[i]<nums2[j]:
-                    ans.append (nums2[j])
-                    break
-                elif j==len(nums2)-1:
-                    ans.append (-1)
-        return ans
-                
+        stack=[]
+        ans=[-1]*len(nums2)
+        for i in range(len(nums2)):
+            if len(stack)==0 or nums2[i]<stack[-1][0]:
+                stack.append([nums2[i],i])
+            else:
+                while stack and nums2[i]>stack[-1][0]:
+                    ans[stack[-1][1]]=(nums2[i])
+                    stack.pop()
+                stack.append([nums2[i],i])
+        for i in range(len(nums1)):
+            nums1[i]=ans[nums2.index(nums1[i])]
+        return nums1
         
