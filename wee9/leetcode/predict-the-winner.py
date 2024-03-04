a83e7838fa2707567b0a9e0a4eb114d698d109a3
Abdulkerim Seid <97250721+Abdulkerim0966@users.noreply.github.com>
2024-03-04 23:49:25 +0300

    Add solution for Predict the Winner

--- wee9/leetcode/predict-the-winner.py
+++ wee9/leetcode/predict-the-winner.py
@@ -1,42 +1,18 @@
 class Solution:
     def predictTheWinner(self, nums: List[int]) -> bool:
-        # total=0
-        # def turninng(num,turn,total):
+        total=0
+        def turninng(num):
   
-        
-        #     if len(num)<=2  and turn==2:
-        #         total-=max(num)
-        #         # print(num,total)
-        #         return total
-        #     if len(num)==1 and turn==1:
+            if len(num)==1 :
                 
-        #         return total
-            
-        #     if turn==2:
+                return num[0]
     
-        #         total-=min(turninng(num[:len(num)-1],1,total),turninng(num[1:len(num)],1,total))
-   
-        #         return total
                 
-                
-                    
-        #     else:
-             
-                
-        #         return max (turninng (num [1:len(num)],2,total),  turninng(num[:len(num)-1],2,total))
-        # ans=turninng(nums,1,total)
-         
-        # return( (sum(nums)-ans)<=ans)
-        def helper(i, j):
-            if i == j:
-                return nums[i]
+            total=max (num[0]-turninng(num [1:len(num)]),num[-1] -turninng(num[:len(num)-1]))
 
-            score = max(nums[j] - helper(i, j-1), nums[i] - helper(i+1, j))
-        
-            return score
-            
-             
-        return helper(0, len(nums)-1) >= 0
+            return total
+        return turninng((nums))>=0
+       
 
 
         
