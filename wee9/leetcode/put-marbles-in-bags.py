2cde4fd81b047359028bb342a2f6705e91516e8e
Abdulkerim Seid <97250721+Abdulkerim0966@users.noreply.github.com>
2024-03-05 10:40:38 +0300

    Add solution for Put Marbles in Bags

---
+++ wee9/leetcode/put-marbles-in-bags.py
@@ -0,0 +1,13 @@
+class Solution:
+    def putMarbles(self, weights: List[int], k: int) -> int:
+    
+        adjacents = []
+
+        for i in range(len(weights) - 1):
+            adjacents.append(weights[i] + weights[i+1])
+        
+        adjacents.sort()
+        min_ = sum(adjacents[:k-1])
+
+        max_ = sum(adjacents[:-k:-1])
+        return max_ - min_
