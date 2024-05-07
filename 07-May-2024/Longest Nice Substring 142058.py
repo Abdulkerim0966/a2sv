e723762f673606183cb4a0870d7fe6bfc2869ee2
Abdulkerim Seid <97250721+Abdulkerim0966@users.noreply.github.com>
2024-05-07 17:09:18 +0300

    Created 'Longest Nice Substring' by A2SV Hub

---
+++ 07-May-2024/Longest Nice Substring 142058.py
@@ -0,0 +1,24 @@
+# Problem: Longest Nice Substring - https://leetcode.com/problems/longest-nice-substring/
+
+class Solution:
+    def longestNiceSubstring(self, s: str) -> str:
+        def checker(st):
+            for i in st:
+                if i.upper() not in st or i.lower() not in st:
+                    return False
+            return True
+        ans=[]
+        for i in range(len(s)-1):
+            for j in range(i+1,len(s)):
+                if checker(s[i:j+1]):
+                    if not ans or len(ans[0])<((j+1)-i):
+                        if ans:
+                            ans.pop()
+                        ans.append(s[i:j+1])
+
+
+        print (ans)
+        return ''.join(ans)       
+        
+
+        
