83eef8d55149114c147e7220b690e66a8e118085
Abdulkerim Seid <97250721+Abdulkerim0966@users.noreply.github.com>
2024-05-09 22:08:09 +0300

    Created 'Fedor and New Game' by A2SV Hub

---
+++ 09-May-2024/Fedor and New Game 144745.py
@@ -0,0 +1,55 @@
+# Problem: Fedor and New Game - https://codeforces.com/contest/467/problem/B
+
+
+
+from gc import get_count
+from re import M
+import sys
+ 
+ 
+def iinp():
+    return int(sys.stdin.readline().strip())
+ 
+ 
+def linp():
+    return list(map(int, sys.stdin.readline().strip().split()))
+ 
+ 
+def lsinp():
+    return sys.stdin.readline().strip().split()
+ 
+ 
+def digit():
+    return [int(i) for i in (list(sys.stdin.readline().strip()))]
+ 
+ 
+def char():
+    return list(sys.stdin.readline().strip())
+ 
+ 
+def string():
+    return sys.stdin.readline().strip()
+ 
+ 
+from collections import Counter, defaultdict, deque
+from bisect import bisect_right, bisect_left
+from math import ceil
+ 
+ 
+def solve():
+    n, m,k = linp()
+ 
+    ar =[]
+    for i in range(m+1):
+        ar.append(iinp())
+    ans =0
+    for j in range(len(ar)-2,-1,-1):
+        if bin(ar[m]^ar[j]).count('1') <=k:
+            ans+=1
+    print(ans)
+    
+ 
+# q = iinp()
+q = 1
+for _ in range(q):
+    solve()
